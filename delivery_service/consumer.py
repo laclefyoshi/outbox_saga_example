b6aab73d6b89f62f3ea82b62fdfa719aee88665c
SAEKI Yoshiyasu <441570+laclefyoshi@users.noreply.github.com>
2021-11-01 17:36:53 +0900

    add simple producer

--- delivery_service/consumer.py
+++ delivery_service/consumer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from kafka import KafkaConsumer
+from kafka import KafkaProducer
 import json
 
 consumer = KafkaConsumer(
@@ -9,6 +10,10 @@ consumer = KafkaConsumer(
      enable_auto_commit=True,
      group_id='delivery-service',
      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+producer = KafkaProducer(bootstrap_servers=['kafka-service:9092'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
 
 for message in consumer:
     message = message.value
--- matching_service/consumer.py
+++ matching_service/consumer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from kafka import KafkaConsumer
+from kafka import KafkaProducer
 import json
 
 consumer = KafkaConsumer(
@@ -10,6 +11,12 @@ consumer = KafkaConsumer(
      group_id='matching-service',
      value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 
+producer = KafkaProducer(bootstrap_servers=['kafka-service:9092'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+
 for message in consumer:
     message = message.value
     print(message)
+
+
